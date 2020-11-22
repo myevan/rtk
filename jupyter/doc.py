24fbde05ab40113c5f38cb3f4e11c60c3754be35
Song Young-Jin <myevan@outlook.com>
2020-11-22 14:00:43 +0000

    doc loading: generals & province

--- jupyter/doc.py
+++ jupyter/doc.py
@@ -1,4 +1,6 @@
 from meta import Singleton
+from scheme import Province
+from scheme import Hero
 
 import os
 
@@ -8,23 +10,24 @@ class DocumentManager(Singleton):
         self.work_dir_path = os.path.realpath(os.path.join(this_dir_path, '..'))
         self.csv_dir_path = os.path.join(self.work_dir_path, 'csvs')
 
-    def load_provinces(self, file_name):
+    def load_table(self, cls, file_name):
         from asset_csv import CSVTable
         csv_file_path = os.path.join(self.csv_dir_path, file_name)
         csv_table = CSVTable.load_file(open(csv_file_path))
 
-        from scheme import Province
         from data import String
-        sch_types = dict(zip(Province.get_field_names(), Province.get_field_types()))
-        csv_types = [sch_types.get(n, String()) for n in csv_table.field_names]
+        sch_types = cls.get_field_name_types()
+        csv_types = [sch_types.get(n, String()) for n in csv_table.heads]
 
-        for record in csv_table.records:
-            values = [t.convert(v) for t, v in zip(csv_types, record)]
-            kwargs = dict(zip(csv_table.field_names, values))
-            Province.put(Province(**kwargs))
+        mem_table = csv_table.convert(csv_types)
+        for row in mem_table.rows:
+            kwargs = dict(zip(csv_table.heads, row))
+            cls.put(cls(**kwargs))
 
 if __name__ == '__main__':
-    DocumentManager.get().load_provinces('1_provinces.csv')
+    doc_mgr = DocumentManager.get()
+    doc_mgr.load_table(Hero, '1_generals.csv')
+    doc_mgr.load_table(Province, '1_provinces.csv')
 
     from scheme import Province
     #print(Province.get(9).head)
